788fee618e0ddd7b5160516b090daf2729a6a0f9
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-06-19 15:28:20 +0530

    Time: 0 ms (100%), Space: 11.8 MB (10.95%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -4,13 +4,14 @@ public:
         vector<int> altitude(gain.size()+1);
         altitude[0]=0;
         int sum = 0;
-        int maxi = INT_MIN;
         for(int i = 0; i < gain.size(); i++){
             sum += gain[i];
             altitude[i + 1] = sum;
-            maxi = max(max(sum , 0) , maxi);
         }
-       
+        int maxi = INT_MIN;
+        for(int i : altitude){
+            if(i > maxi)maxi = i;
+        }
         return maxi;
     }
 };
