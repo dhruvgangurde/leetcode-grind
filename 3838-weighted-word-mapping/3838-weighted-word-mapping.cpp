4f3efc6fdd8a287bffd7da220e02bb6d65707dcf
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-06-19 17:23:50 +0530

    Time: 0 ms (100%), Space: 43.5 MB (97.38%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string result = "";
+        for(string s : words){
+            int sum = 0;
+            for (char c : s) {
+                int alphabet = c - 'a';
+                sum += weights[alphabet];
+            }
+            result += char('z' - sum%26);
+        }
+        return result;
+    }
+};
