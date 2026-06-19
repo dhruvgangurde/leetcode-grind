c342621570bfec34933655be6d7783863223a213
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-06-19 17:52:33 +0530

    Time: 0 ms (100%), Space: 27.8 MB (16.04%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-    int sumEm(int num){
-        int ans = 0;
-        while(num > 0){
-            ans += num % 10;
-            num /= 10;
-        }
-        return ans;
-    }
     int minElement(vector<int>& nums) {
-        int mini = INT_MAX;
-        for(int i = 0; i < nums.size(); i++){
-            nums[i] = sumEm(nums[i]);
-            mini = min(nums[i], mini);
+        int res = 36;
+        for(auto& n : nums){
+            res = min(res,n - 9*((n/10) + (n/100) + (n/1000) + (n/10000)));
         }
-        return mini;
+        return res;
     }
 };
