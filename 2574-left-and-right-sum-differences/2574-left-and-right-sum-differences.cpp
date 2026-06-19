daa70b16c2d7be546bb0551c9d55ebd88d17a8c8
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-06-19 15:56:51 +0530

    Time: 0 ms (100%), Space: 14 MB (99.99%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -16,3 +16,22 @@ public:
         return nums;
     }
 };
+#pragma GCC optimize("Ofast")
+static constexpr size_t BUFFER_SIZE = 64ull << 20;
+alignas(std::max_align_t)
+static unsigned char BUFFER[BUFFER_SIZE];
+static unsigned char* ptr = BUFFER;
+void* operator new(std::size_t n) {
+    constexpr size_t A = alignof(std::max_align_t);
+    n = (n + A - 1) & ~(A - 1);
+    if (ptr + n > BUFFER + BUFFER_SIZE)throw std::bad_alloc();
+    void* r = ptr;    ptr += n;
+    return r;
+}
+void* operator new[](std::size_t n) {
+    return operator new(n);
+}
+void operator delete(void*) noexcept {}
+void operator delete[](void*) noexcept {}
+void operator delete(void*, std::size_t) noexcept {}
+void operator delete[](void*, std::size_t) noexcept {}
