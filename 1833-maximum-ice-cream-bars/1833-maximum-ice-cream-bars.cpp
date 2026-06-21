20fd387097a49e86f3e0eefefb90f9ed4dfff0ac
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-06-21 19:47:58 +0530

    Time: 8 ms (86.81%), Space: 84.4 MB (11.94%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        const int LARGEST_INT = 100000;
-        vector<int> freq(LARGEST_INT + 1, 0);
+        int maxCost = 0;
+        for(auto cost : costs){
+            maxCost = max(maxCost, cost);
+        }
+        vector<int> freq(maxCost + 1, 0);
         for(auto a : costs){
             freq[(a)]++;
         }
         int maxi = 0;
-        for(int price = 1; price <= LARGEST_INT; price++){
+        for(int price = 1; price <= maxCost; price++){
             if(freq[price] == 0)continue;
             int canBuy = min(freq[price] , coins/price);
             maxi += canBuy;
