798e116dc84c862d4ede4b1738127030ad49041b
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-06-22 15:50:40 +0530

    Time: 0 ms (100%), Space: 11.6 MB (83.84%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        if(nums.size()==0)return 0;
-        int left=0;
-        int right=nums.size()-1;
-        int count=0;
-        while(left<=right){
-            while(left<=right && nums[left]!=val)left++;
-            while(left<=right && nums[right]==val)right--;
-            if(left<right)
-            swap(nums[left++],nums[right--]);
-
-            
+        int left = 0;//will find the 1st occurence of val in the nums
+        int right = nums.size() - 1;//will find the first occurence of non-val in nums
+        //after finding right positions for left and right we swap them
+        int count = 0;
+        while(left <= right){
+            while(left <= right && nums[left] != val){
+                left++;
+            }
+            while(left <= right && nums[right] == val){
+                right--;
+            }
+            if(left < right){
+                swap(nums[left++],nums[right--]);
+            }
         }
         return left;
     }
