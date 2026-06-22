ba86cc6e59cbd5950e764f3b20ba5a58cdf9163d
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-06-22 15:41:21 +0530

    Time: 3 ms (28.93%), Space: 9.9 MB (77.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string newString = "";
-        for(char c : s){
-            int x = (int) c;
-            if( 65 <= x && x <= 90){
-                //capital letter
-                x += 32;
-                newString += (char)x;
-            }else if(97 <= x && x <= 122){
-                //lowercase letter
-                newString += (char)x;
-            }else if(48 <= x && x <= 57){
-                //(0-9)
-                newString += (char)x;
-            }else{
-                continue;
-            }
-        }
         int left = 0;
-        int right = newString.size() - 1;
-        while(left <= right){
-            if(newString[left] != newString[right]){
+        int right = s.size() - 1;
+
+        while(left < right) {
+            while(left < right && !isalnum(s[left]))
+                left++;
+
+            while(left < right && !isalnum(s[right]))
+                right--;
+
+            if(tolower(s[left]) != tolower(s[right]))
                 return false;
-            }
-            left ++;
+
+            left++;
             right--;
         }
+
         return true;
     }
 };
