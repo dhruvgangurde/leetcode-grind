b5f1d0ea08d63de2bdebe21c8ecf2fbcbea031ce
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-06-25 14:47:35 +0530

    Time: 4 ms (65.3%), Space: 13.4 MB (80.4%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        int left = 0;
-        int right = n;
         vector<int> ans;
-        while(left < n && right < nums.size()){
-            ans.push_back(nums[left++]);
-            ans.push_back(nums[right++]);
+        for(int i = 0; i < n; i++){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i + n]);
         }
         return ans;
     }
