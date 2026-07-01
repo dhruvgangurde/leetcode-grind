158444c36099027f28435e39448ac877db798fbb
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-07-01 23:21:47 +0530

    Time: 8 ms (55.3%), Space: 23 MB (13.14%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -2,26 +2,36 @@ class Solution {
 public:
     bool isPossible(vector<int>& piles, int h, int hourly){
         long long totalhours = 0;
-        for(int i : piles){
-            totalhours += ceil((double)i / hourly);
+        //calculate total hours required if koko eats 'hourly' bananas every hour
+        for(int bananas : piles){
+            totalhours += ceil((double)bananas / hourly);
         }
-        return totalhours <= h ;
+        //check if koko can finish within h hours
+        return totalhours <= h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
+        //find the maximum pile
         int maxi = INT_MIN;
-        for(int i : piles)
-            maxi = max(i, maxi);
+        for(int bananas : piles)
+            maxi = max(bananas, maxi);
+        //minimum possible speed = 1
+        //maximum possible speed = largest pile
         int low = 1, high = maxi;
         int ans = 0;
         while(low <= high){
             int mid = (low + high) / 2;
             if(isPossible(piles, h, mid)){
+                //mid is a possible answer
+                //try to minimize the eating speed
                 ans = mid;
-                high = mid -1;
+                high = mid - 1;
             }else{
+                //mid is too slow
+                //increase the eating speed
                 low = mid + 1;
             }
         }
+        //minimum eating speed required
         return ans;
     }
 };
