608f4388a6afea5a14737abe1795991468822361
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-07-01 23:23:01 +0530

    Time: 0 ms (100%), Space: 22.8 MB (79.19%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,37 +1,49 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& piles, int h, int hourly){
-        long long totalhours = 0;
-        //calculate total hours required if koko eats 'hourly' bananas every hour
-        for(int bananas : piles){
-            totalhours += ceil((double)bananas / hourly);
+
+    int maxElement(vector <int> & piles)
+    {
+        int max=0;
+        for(int ele : piles)
+        {
+            if (ele>max) max=ele;
         }
-        //check if koko can finish within h hours
-        return totalhours <= h;
+        return max;
+    }
+
+    bool isValid(vector <int>& piles, int k,int h)
+    {
+        long long hrs=0;
+        for ( int x : piles)
+        {
+            hrs+=(x+k-1)/k;
+        }
+        if(hrs<=h)
+        {
+            return true;
+        }
+        return false;
+        
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        //find the maximum pile
-        int maxi = INT_MIN;
-        for(int bananas : piles)
-            maxi = max(bananas, maxi);
-        //minimum possible speed = 1
-        //maximum possible speed = largest pile
-        int low = 1, high = maxi;
-        int ans = 0;
-        while(low <= high){
-            int mid = (low + high) / 2;
-            if(isPossible(piles, h, mid)){
-                //mid is a possible answer
-                //try to minimize the eating speed
-                ans = mid;
-                high = mid - 1;
-            }else{
-                //mid is too slow
-                //increase the eating speed
-                low = mid + 1;
+        int low=1;
+        int high = maxElement(piles);
+        int ans=high;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(isValid(piles,mid,h))
+            {
+                ans=mid;
+                high=mid-1;
             }
+            else
+            {
+                low=mid+1;
+            }
+
         }
-        //minimum eating speed required
         return ans;
     }
+
 };
