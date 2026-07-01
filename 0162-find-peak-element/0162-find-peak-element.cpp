7899a617e609a986dd92f8d3f7644c2d22b4fe7b
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-07-01 21:44:42 +0530

    Time: 0 ms (100%), Space: 12.5 MB (71.88%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+        /* O(n) solution
         if(nums.size() == 1) 
             return 0;
         if(nums[nums.size() - 1] > nums[nums.size() - 2])
@@ -10,5 +11,25 @@ public:
                 return i;
         }
         return 0;
+        */
+        if(nums.size() == 1) 
+            return 0;
+        if(nums[0] > nums[1]) 
+            return 0;
+        if(nums[nums.size() - 1] > nums[nums.size() - 2])
+            return nums.size() - 1;
+        int low = 1, high = nums.size() - 2;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])  
+                return mid;
+            else if(nums[mid] > nums[mid - 1])
+                // increasing trend
+                low = mid + 1;
+            else
+                // decreasing trend
+                high = mid - 1;
+        }
+        return -1;
     }
 };
