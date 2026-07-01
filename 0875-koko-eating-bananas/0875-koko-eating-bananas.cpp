a9bec1c7cd6366849fa35236b46cda90446fcdf8
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-07-01 23:24:52 +0530

    Time: 16 ms (13.66%), Space: 23 MB (13.14%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
-
-    int maxElement(vector <int> & piles)
-    {
-        int max=0;
-        for(int ele : piles)
-        {
-            if (ele>max) max=ele;
+    bool isPossible(vector<int>& piles, int h, int hourly){
+        long long totalhours = 0;
+        //calculate total hours required if koko eats 'hourly' bananas every hour
+        for(int bananas : piles){
+            totalhours += ceil((double)bananas / hourly);
         }
-        return max;
-    }
-
-    bool isValid(vector <int>& piles, int k,int h)
-    {
-        long long hrs=0;
-        for ( int x : piles)
-        {
-            hrs+=(x+k-1)/k;
-        }
-        if(hrs<=h)
-        {
-            return true;
-        }
-        return false;
-        
+        //check if koko can finish within h hours
+        return totalhours <= h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low=1;
-        int high = maxElement(piles);
-        int ans=high;
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            if(isValid(piles,mid,h))
-            {
-                ans=mid;
-                high=mid-1;
+        //find the maximum pile
+        int maxi = INT_MIN;
+        for(int bananas : piles)
+            maxi = max(bananas, maxi);
+        //minimum possible speed = 1
+        //maximum possible speed = largest pile
+        int low = 1, high = maxi;
+        int ans = 0;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(isPossible(piles, h, mid)){
+                //mid is a possible answer
+                //try to minimize the eating speed
+                ans = mid;
+                high = mid - 1;
+            }else{
+                //mid is too slow
+                //increase the eating speed
+                low = mid + 1;
             }
-            else
-            {
-                low=mid+1;
-            }
-
         }
+        //minimum eating speed required
         return ans;
     }
-
 };
