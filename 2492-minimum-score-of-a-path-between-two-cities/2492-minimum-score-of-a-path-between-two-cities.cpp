bde3113d207036cb14ac30d9010f3afa9b6168de
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-07-04 19:14:06 +0530

    Time: 55 ms (72.97%), Space: 132.7 MB (77.98%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
+        // adjacency list -> {neighbour, edge cost}
         vector<vector<pair<int, int>>> g(n + 1);
+        // build the graph
         for(int i = 0; i < roads.size(); i++) {
             int u = roads[i][0];
             int v = roads[i][1];
             int w = roads[i][2];
-            g[u].push_back({v, w});// for edge u -> v with cost w
-            g[v].push_back({u, w});// for edge v -> u with cost w
+            g[u].push_back({v, w}); // for edge u -> v with cost w
+            g[v].push_back({u, w}); // for edge v -> u with cost w
         }
-        vector<bool> visited(n + 1,false);
+        vector<bool> visited(n + 1, false);
         queue<int> q;
-        q.push(1);
-        visited[1] = true;
-        int ans = INT_MAX;
+        q.push(1);// start BFS from city 1
+        visited[1] = true;// mark city 1 as visited
+        int ans = INT_MAX;// stores the minimum edge cost
+        // BFS traversal
         while(!q.empty()) {
-            int u = q.front();
+            int u = q.front();// current city
             q.pop();
-            for(auto [neighbour, cost] : g[u]){
-                ans = min(ans, cost);
-                if(!visited[neighbour]){
-                    visited[neighbour] = true;
-                    q.push(neighbour);
+            // visit all neighbours of current city
+            for(auto [neighbour, cost] : g[u]) {
+                ans = min(ans, cost); // update minimum edge cost
+                // visit neighbour only once
+                if(!visited[neighbour]) {
+                    visited[neighbour] = true; // mark neighbour as visited
+                    q.push(neighbour);         // add neighbour to queue
                 }
             }
         }
-        return ans;
+        return ans; // minimum edge cost in the connected component of city 1
     }
 };
