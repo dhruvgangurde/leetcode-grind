d9154f3c632568ce9f69963b507701cfe8b91e98
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-07-07 21:41:44 +0530

    Time: 7 ms (55.52%), Space: 12.5 MB (22.64%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -1,7 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        node->val = node->next->val;      // copy next node's value
-        node->next = node->next->next;    // skip the next node
+        // We cannot delete this node directly because we don't have access
+        // to the previous node.
+        // Instead, copy the value of the next node into the current node,
+        // then bypass the next node by updating the current node's next pointer.
+        // Effectively, the next node is removed from the list, and the current node now represents the next node's value.
+        node->val = node->next->val;
+        node->next = node->next->next;
     }
 };
