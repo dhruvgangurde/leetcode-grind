d099c0892d089d8000c73a9c6f5837aa88709975
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-07-07 19:14:12 +0530

    Time: 0 ms (100%), Space: 50.7 MB (5.58%) - LeetHub

---
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int f(vector<int> nums){
+        int maxi = 0;
+        int index;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > maxi) {
+                maxi = nums[i];
+                index = i;
+            }
+        }
+        return index;
+        
+    }
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        // for each row , we find the find max element in that as it has the most chance of being the peak element
+        // if we dont find a peak element, then move in the direction of where the bigger element is present wrt our max element -- elimination
+        int n = mat.size();
+        int low = 0, high = mat.size() - 1;// for keeping track of row on which we are
+        while(low <= high) {
+            int mid = (low + high) / 2;// middle row
+            int maybePeak = f(mat[mid]);
+            int value = mat[mid][maybePeak];
+            int up, down;
+            // for checking down
+            if(mid + 1 < n){
+                down = mat[mid + 1][maybePeak]; 
+            }else{
+                down = -1;
+            }
+            // for checking up
+            if(mid - 1 >= 0){
+                up = mat[mid - 1][maybePeak];
+            }else{
+                up = -1;
+            }
+            if(up < value && down < value)
+                return {mid, maybePeak};
+            if(up > value)
+                high = mid - 1;
+            if(down > value)
+                low = mid + 1;
+        }
+        return {-1, -1};
+    }
+};
