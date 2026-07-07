6dd1754ccd7a2b4ce3d8e90c3c91e8e21862d7b6
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-07-07 19:22:27 +0530

    Time: 0 ms (100%), Space: 50.2 MB (48.67%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int f(const vector<int> &nums){
-        int maxi = 0;
-        int index = -1;
-        for (int i = 0; i < nums.size(); i++) {
+    pair<int, int> findMax(const vector<int> &nums){
+        int index = 0;
+        int maxi = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
             if (nums[i] > maxi) {
                 maxi = nums[i];
                 index = i;
             }
         }
-        return index;
-        
+        return {index, maxi};
     }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         // for each row , we find the find max element in that as it has the most chance of being the peak element
@@ -18,9 +17,8 @@ public:
         int n = mat.size();
         int low = 0, high = mat.size() - 1;// for keeping track of row on which we are
         while(low <= high) {
-            int mid = (low + high) / 2;// middle row
-            int maybePeak = f(mat[mid]);// index of maybe peak element
-            int value = mat[mid][maybePeak];// value of that peak element
+            int mid = low + (high - low) / 2;// middle row
+            auto [maybePeak, value] = findMax(mat[mid]);
             int up, down;
             // for checking down
             if(mid + 1 < n){
@@ -38,7 +36,7 @@ public:
                 return {mid, maybePeak};
             if(up > value)
                 high = mid - 1;
-            if(down > value)
+            else
                 low = mid + 1;
         }
         return {-1, -1};
