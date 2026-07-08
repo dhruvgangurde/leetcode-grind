5997631f0a1d7ccef0c91386583db54bcbf70955
Dhruv Gangurde <dhruvgangurde13@gmail.com>
2026-07-08 16:05:06 +0530

    Time: 10 ms (46.23%), Space: 11.4 MB (54.87%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,6 +6,8 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+ /*
+uses extra space
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
@@ -20,5 +22,33 @@ public:
             temp = temp->next;
         }
         return nullptr;
+    }
+};
+*/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head, ListNode* &fast, ListNode* &slow) {
+        if(head == NULL || head->next == NULL)
+            return false;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow)
+                return true;   
+        }
+        return false;
+    }
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        if(!hasCycle(head, fast, slow))
+            return nullptr;
+        slow = head;
+        while(fast != slow) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return slow;
     }
 };
